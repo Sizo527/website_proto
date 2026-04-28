f4728e9452475810f7652016af6169d3aaceba09
Local Backup <backup@local.dev>
2026-04-28 20:31:01 +0200

    fix-base-path-for-renamed-repo

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: mode === 'production' ? '/mtshabezihighwebsite/' : '/',
+    base: mode === 'production' ? '/website_proto/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
